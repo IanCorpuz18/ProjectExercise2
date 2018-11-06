0923893a9e91cf5b28a5d9d937a5000991b6aac5
IanCorpuz18 <bc00009jc@gmail.com>
2018-11-06 08:54:53 +0800

    animanga

--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -6,7 +6,7 @@ import {
     Text,
     View,
     Button,
-TextInput
+    TextInput
 } from 'react-native';
 import Icons from 'react-native-vector-icons/Ionicons';
 import { Header, Left, Right } from 'native-base';
@@ -15,72 +15,72 @@ import CustomIcon from '../assets/components/CustomIcon';
 import CustomInput from '../assets/components/customInput';
 class LoginScreen extends Component {
     static navigationOptions = {
-        header:null
+        header: null
     };
-    state={
-        email:"",
-        pass:""
+    state = {
+        email: "",
+        pass: ""
     }
-    
-inputEmail=(val) =>{
-this.setState({email:val})
 
-}
-inputPass=(val) =>{
-    this.setState({pass:val})
-    
+    inputEmail = (val) => {
+        this.setState({ email: val })
+
     }
-    
-    logIn = () =>{
-        if(this.state.email==='' && this.state.pass===''){
+    inputPass = (val) => {
+        this.setState({ pass: val })
+
+    }
+
+    logIn = () => {
+        if (this.state.email === '' && this.state.pass === '') {
             alert('Please enter your email and password.')
         }
-        else if(this.state.email===''){
+        else if (this.state.email === '') {
             alert('Please enter your email.')
         }
-        else if(this.state.pass===''){
-          alert('Please enter your password.')
+        else if (this.state.pass === '') {
+            alert('Please enter your password.')
         }
-      
-        else{
-          this.setState({
-            email: "",
-            pass: ""
-          });
-          this.props.navigation.navigate('Explore')
-      
+
+        else {
+            this.setState({
+                email: "",
+                pass: ""
+            });
+            this.props.navigation.navigate('Explore')
+
         }
-          
-        }
+
+    }
     render() {
         return (
-            <View style={{flex:1, backgroundColor:"#00A795"}}>
-            <Header style={{ backgroundColor:"#00A795"}}>
-            <Left style={{ marginLeft: "-40%" }}>
-                <Icons name='ios-arrow-back' size={30} color={"white"}  style={{ paddingRight: 50 }}
-                    onPress={() => this.props.navigation.goBack()} />
-            </Left>
-        </Header> 
-               
-            <View style={styles.container}>
-                <Text  style={styles.welcome}>
-                    Login
-        </Text>
-        <Text></Text>
-                <Text style={{color:"white", marginLeft:"10%"}}>Email </Text>
-                <CustomInput style={{color:"white"}}   onChangeText={text => this.inputEmail(text)}
-                         value={this.state.email} />
-                <Text> </Text>
-                <Text style={{color:"white",marginLeft:"10%"}}>Password</Text>
-                <ShowPass  onChangeText={text => this.inputPass(text)} 
-                value={this.state.pass}/>
-                <Text> </Text>
-                <CustomIcon onPress={this.logIn}/>
+            <View style={{ flex: 1, backgroundColor: "#00A795" }}>
+                <Header style={{ backgroundColor: "#00A795" }}>
+                    <Left style={{ marginLeft: "-40%" }}>
+                        <Icons name='ios-arrow-back' size={30} color={"white"} style={{ paddingRight: 50 }}
+                            onPress={() => this.props.navigation.goBack()} />
+                    </Left>
+                </Header>
 
+                <View style={styles.container}>
+                    <Text style={styles.welcome}>
+                        Login
+        </Text>
+                    <Text></Text>
+                    <Text style={{ color: "white", marginLeft: "10%" }}>Email </Text>
+                    <CustomInput style={{ color: "white" }} onChangeText={text => this.inputEmail(text)}
+                        value={this.state.email} />
+                    <Text> </Text>
+                    <Text style={{ color: "white", marginLeft: "10%" }}>Password</Text>
+                    <ShowPass onChangeText={text => this.inputPass(text)}
+                        value={this.state.pass} />
+                    <Text> </Text>
+                    <CustomIcon onPress={this.logIn} />
+
+                </View>
             </View>
-            </View>
-          
-    );
+
+        );
     }
 }
 
@@ -96,7 +96,7 @@ const styles = StyleSheet.create({
         // textAlign: 'center',
         margin: 10,
         color: "white",
-        marginLeft:"10%"
+        marginLeft: "10%"
     },
     instructions: {
         textAlign: 'center',
